52a3404bd5036f18af5e7895ed7030bd2d48d63b
Eric0926 <38851446+Eric0926@users.noreply.github.com>
2020-05-16 00:08:09 -0400

    Update form.js

--- static/js/form.js
+++ static/js/form.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 			}
 			else {
 				$('#successAlert').text(data.results).show();
-				$('#img2').prop("src", data.results);
+				$('#img2').prop("src", data.results).show();
 				$('#errorAlert').hide();
 			}
 
