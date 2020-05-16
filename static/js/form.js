96cd00607f1f5ffff22f0f2c2c4310eedaa0de8f
Eric0926 <38851446+Eric0926@users.noreply.github.com>
2020-05-15 20:43:48 -0400

    Add files via upload

---
+++ static/js/form.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$('form').on('submit', function(event) {
+
+		$.ajax({
+			data : {
+				image : $('#fname').val(),
+			},
+			type : 'POST',
+			url : '/search'
+		})
+		.done(function(data) {
+
+			if (data.error) {
+				$('#errorAlert').text(data.error).show();
+				$('#successAlert').hide();
+			}
+			else {
+				$('#successAlert').text(data.results).show();
+				$('#errorAlert').hide();
+			}
+
+		});
+
+		event.preventDefault();
+
+	});
+
+});
